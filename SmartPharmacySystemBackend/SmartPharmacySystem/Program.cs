f460b9060851a0a8f0fe3b0509634eb53492727e
ProgramerShaher <shahrkhaldalyryalyry@gmail.com>
2026-08-21 00:09:05 +0300

    chore: initialize WhatsApp session cache and add superpower development infrastructure and test suite

--- SmartPharmacySystemBackend/SmartPharmacySystem/Program.cs
+++ SmartPharmacySystemBackend/SmartPharmacySystem/Program.cs
@@ -21,7 +21,14 @@ var builder = WebApplication.CreateBuilder(args);
 
 // -------------------- Database --------------------
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"),
+        sqlServerOptionsAction: sqlOptions =>
+        {
+            sqlOptions.EnableRetryOnFailure(
+                maxRetryCount: 5,
+                maxRetryDelay: TimeSpan.FromSeconds(30),
+                errorNumbersToAdd: null);
+        }));
 builder.Services.AddScoped<IApplicationDbContext>(provider => provider.GetRequiredService<ApplicationDbContext>());
 
 // -------------------- JWT Authentication --------------------
